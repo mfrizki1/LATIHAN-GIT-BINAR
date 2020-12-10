70776fc66226a9afae0864ff7302125732f7e524
berkahsuksesteknologi <68946329+berkahsuksesteknologi@users.noreply.github.com>
2020-12-10 21:08:51 +0800

    second commit

--- src/main.kt
+++ src/main.kt
@@ -1,22 +1,5 @@
 fun main() {
-//    println("Hello World")
-//    val a = 0
-//    if(a == 1){
-//        println("1")
-//
-//    }else{
-//        println("abcs")
-//
-//    }
-//    var baris = arrayListOf("1","2","3","4","5")
-//    var kolom = arrayListOf("6","7","8","9","10")
-//
-//    for (i in 0 until baris.size){
-//        println(i)
-//        for(j in i..kolom.size){
-//            print(j)
-//        }
-//    }
+
 
     print("1225441:")
     separateNumber(1225441)

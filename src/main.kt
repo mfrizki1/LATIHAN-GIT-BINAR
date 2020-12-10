13edf4c66d6ff79277ff657beee79e6c7c290320
berkahsuksesteknologi <mfrizki1>
2020-12-10 21:41:14 +0800

    Added code dari Faisal

--- src/main.kt
+++ src/main.kt
@@ -1,26 +1,6 @@
 fun main() {
+// CODE DARI FAISAL
+    println("Code dari Faisal")
 
 
-    print("1225441:")
-    separateNumber(1225441)
-    println("")
-    println("3579:")
-    separateNumber(3579)
-
-
-}
-
-fun separateNumber(number: Int) {
-    println("")
-    val numberString:String = number.toString()
-    for (i in numberString.indices) {
-        var stringNumber:String = numberString[i].toString()
-//        println(stringNumber)
-        val zeroCount = (numberString.length - 1) - i
-//        print("tes zero Count $zeroCount : ")
-        for (j in 0 until zeroCount) {
-            stringNumber += "0"
-        }
-        println(stringNumber)
-    }
 }
